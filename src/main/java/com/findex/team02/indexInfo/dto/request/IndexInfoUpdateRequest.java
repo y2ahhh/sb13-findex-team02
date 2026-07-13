baa12cdfa15e3ac8edf121eee81f0681cb4d6b38
줴령 <gpfud09@gmail.com>
2026-07-13 17:46:46 +0900

    refactor: 담당 범위 외 클래스 삭제

--- src/main/java/com/findex/team02/indexInfo/dto/request/IndexInfoUpdateRequest.java
+++
@@ -1,9 +0,0 @@
-package com.findex.team02.indexInfo.dto.request;
-
-public record IndexInfoUpdateRequest (
-    Integer employedItemsCount,
-    String basePointInTIme,
-    Integer baseIndex,
-    boolean favorite
-) {
-}
