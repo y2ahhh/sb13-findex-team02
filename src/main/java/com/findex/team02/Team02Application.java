7ab133bb102f65387aacb13948786018d5f1ff27
sungwoong-svg <skarn506@gmail.com>
2026-07-13 14:27:21 +0900

    chore: 프로젝트 설정 및 애플리케이션 클래스 복구

--- src/main/java/com/findex/team02/Team02Application.java
+++ src/main/java/com/findex/team02/Team02Application.java
@@ -1,8 +1,12 @@
 package com.findex.team02;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Slf4j
+@EnableJpaAuditing
 @SpringBootApplication
 public class Team02Application {
 
